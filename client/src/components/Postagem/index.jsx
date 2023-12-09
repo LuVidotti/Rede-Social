f0573ec5bf5f7d857cfe6a9f71f151c21cdc48e4
Luís Felipe Vidotti <luisfelipealmeida66@gmail.com>
2023-12-09 02:58:01 -0300

    criando pagina de login

--- client/src/components/Postagem/index.jsx
+++ client/src/components/Postagem/index.jsx
@@ -1,9 +1,14 @@
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row"
+
 function Postagem() {
     return(
         <div className="container form-control mt-4">
             <h6>LuVidotti:</h6>
             <p>aqui eh corinthians caralho</p>
-            <small>publicado em: 22/12/2023</small>
+            <small className="d-block mb-2">publicado em: 22/12/2023</small>
+            <button className="btn btn-dark btn-sm">Responder</button>
         </div>
     )
 }
--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,10 +1,27 @@
 import Header from "../../components/Header";
+import { Container, Form } from "react-bootstrap";
+import Titulo from "../../components/Titulo";
 
 function Login() {
     return (
-        <div>
+        <div className="bg-light vh-100">
             <Header />
-            <h1>Pagina Login</h1>
+            <Container style={{textAlign: "center"}} className="mb-4">
+                <Titulo titulo="Login"/>
+            </Container>
+            <Container className="d-flex align-items-center justify-content-center mt-4">
+                <Form className="bg-dark" style={{color: "#FFF", padding: "16px", borderRadius: "16px", width: "30vw"}}>
+                    <Form.Group className="mb-3" controlId="formGroupEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Digite seu email..." />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupPassword">
+                        <Form.Label>Senha</Form.Label>
+                        <Form.Control type="password" placeholder="Digite sua senha..." />
+                    </Form.Group>
+                    <button type="submit" className="btn btn-primary mt-2">Entrar</button>
+                </Form>
+            </Container>
         </div>
     )
 }
