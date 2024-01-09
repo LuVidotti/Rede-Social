a04939bc5e1e6c363d13ef215c45d93a215e6d91
Luís Felipe Vidotti <luisfelipealmeida66@gmail.com>
2024-01-09 18:43:10 -0300

    criando api de respostas as postagens

--- server/routes/postagens.js
+++ server/routes/postagens.js
@@ -4,12 +4,20 @@ const mongoose = require("mongoose");
 require('../models/Postagem');
 const Postagem = mongoose.model("postagens");
 const verificaToken = require('../auth');
-require('../models/Usuario');
-const Usuario = mongoose.model('usuarios');
+require('../models/Resposta');
+const Resposta = mongoose.model('respostas');
 
 router.get('/', (req,res) => {
-    Postagem.find().then((postagens) => {
+    Postagem.find().populate('autorId').then((postagens) => {
         res.status(200).json(postagens);
+    }).catch((erro) => {
+        res.status(500).json({errorMessage: "Erro interno no servidor", erro:erro});
+    })
+})
+
+router.get('/:id', (req,res) => {
+    Postagem.findOne({_id: req.params.id}).then((postagem) => {
+        res.status(200).json(postagem);
     }).catch((erro) => {
         res.status(500).json({errorMessage: "Erro interno no servidor", erro:erro});
     })
@@ -43,6 +51,42 @@ router.post('/', verificaToken, (req,res) => {
 router.delete('/:idPostagem', verificaToken, (req,res) => {
     Postagem.deleteOne({_id: req.params.idPostagem}).then(() => {
         res.status(200).json({message: "Postagem deletada com sucesso!!!"});
+    }).catch((erro) => {
+        res.status(500).json({errorMessage: "Erro interno no servidor", erro:erro});
+    })
+})
+
+//Respostas
+
+router.post('/respostas', verificaToken, (req,res) => {
+    const user = req.user;
+    let erros = [];
+
+    if(!req.body.conteudo || typeof req.body.conteudo === undefined || req.body.conteudo === null) {
+        erros.push("Erro, conteudo invalido");
+    }
+
+    if(erros.length > 0) {
+        res.status(402).json(erros);
+    } else {
+        const novaResposta = {
+            autorId: user._id,
+            autorNome: user.nome,
+            postagemId: req.body.postagemId,
+            conteudo: req.body.conteudo
+        }
+
+        new Resposta(novaResposta).save().then((respostaSalva) => {
+            res.status(201).json({message: "Resposta adicionada com sucesso!!!", respostaSalva:respostaSalva});
+        }).catch((erro) => {
+            res.status(500).json({errorMessage: "Erro interno no servidor", erro:erro});
+        })
+    }
+})
+
+router.get('/respostas/:postagemId', (req,res) => {
+    Resposta.find({postagemId: req.params.postagemId}).populate('postagemId').then((respostas) => {
+        res.status(200).json(respostas);
     }).catch((erro) => {
         res.status(500).json({errorMessage: "Erro interno no servidor", erro:erro});
     })
